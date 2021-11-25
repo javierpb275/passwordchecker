1a7fa9ffe3b387697aca0f93e1fb22f17911dc2d
javierpb275 <javierpb275@gmail.com>
2021-11-25 17:16:05 +0100

    password checker

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
+import hashlib
 
 if __name__ == '__main__':
-    url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA' #hashed password
-    res = requests.get(url)
-    print(res)#<Response [200]>
+
+
+    def request_api_data(query_char):
+        url = 'https://api.pwnedpasswords.com/range/' + query_char
+        res = requests.get(url)
+        if res.status_code != 200:
+            raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+        return res
+
+    def pwned_api_check(password):
+        sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+        return sha1password
+
+
+    #request_api_data('123')
+    pwned_api_check('123')
