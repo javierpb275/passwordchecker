c5549d722ad12e0670f9acdbeccf8882ee38207d
javierpb275 <javierpb275@gmail.com>
2021-11-25 16:50:17 +0100

    password api

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
 
 if __name__ == '__main__':
-    pass
+    url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA' #hashed password
+    res = requests.get(url)
+    print(res)#<Response [200]>
